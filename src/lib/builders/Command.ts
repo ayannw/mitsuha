5df21d575c6f0e66ddd0401bc6b948e9b6ad6eb0
ayannw <ayanispro420@gmail.com>
2021-04-03 00:11:19 +0600

    better Command builder

--- src/lib/builders/Command.ts
+++ src/lib/builders/Command.ts
@@ -12,30 +12,37 @@ interface CommandOptions {
     help?: string;
     category?: string;
     permissions?: string[];
+    argumentError?: string;
     ownerOnly?: boolean;
 }
 
 export interface Command extends CommandOptions {
+	__type: string;
     name: string;
     exec: Exec;
 }
 
 export class Command implements Command {
+	public __type: string;
     public name: string;
     public exec: Exec;
     public aliases: string[];
     public help: string;
     public category: string;
     public permissions: string[];
+    public argumentError: string;
     public ownerOnly: boolean;
 
     public constructor(name: string, opts: CommandOptions, exec: Exec) {
+		this.__type = 'Command';
         this.name = name;
         this.exec = exec;
-        this.aliases = opts.aliases || [];
         this.help = opts.help || '*Description unavailable.*';
         this.category = opts.category || 'Uncategorized';
-        this.permissions = opts.permissions || [];
-        this.ownerOnly = opts.ownerOnly || false;
+
+		opts.aliases ? (this.aliases = opts.aliases) : null;
+        opts.permissions ? (this.permissions = opts.permissions) : null;
+        opts.argumentError ? (this.argumentError = opts.argumentError) : null;
+        opts.ownerOnly ? (this.ownerOnly = true) : null;
     }
 }
