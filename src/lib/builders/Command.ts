37e22802cdf37d6f607400f99dc630403695c6e4
ayannw <ayanispro420@gmail.com>
2021-04-03 00:30:52 +0600

    update command builder

--- src/lib/builders/Command.ts
+++ src/lib/builders/Command.ts
@@ -12,37 +12,37 @@ interface CommandOptions {
     help?: string;
     category?: string;
     permissions?: string[];
-    argumentError?: string;
+    permissionError?: string;
     ownerOnly?: boolean;
 }
 
 export interface Command extends CommandOptions {
-	__type: string;
+    __type: string;
     name: string;
     exec: Exec;
 }
 
 export class Command implements Command {
-	public __type: string;
+    public __type: string;
     public name: string;
     public exec: Exec;
     public aliases: string[];
     public help: string;
     public category: string;
     public permissions: string[];
-    public argumentError: string;
+    public permissionError: string;
     public ownerOnly: boolean;
 
     public constructor(name: string, opts: CommandOptions, exec: Exec) {
-		this.__type = 'Command';
+        this.__type = 'Command';
         this.name = name;
         this.exec = exec;
         this.help = opts.help || '*Description unavailable.*';
         this.category = opts.category || 'Uncategorized';
 
-		opts.aliases ? (this.aliases = opts.aliases) : null;
+        opts.aliases ? (this.aliases = opts.aliases) : null;
         opts.permissions ? (this.permissions = opts.permissions) : null;
-        opts.argumentError ? (this.argumentError = opts.argumentError) : null;
+        opts.permissionError? (this.permissionError = opts.permissionError) : null;
         opts.ownerOnly ? (this.ownerOnly = true) : null;
     }
 }
