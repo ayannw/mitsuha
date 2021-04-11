aa5ddb159d90a661ed1dba8114a9d767fd03008f
ayannw <ayanispro420@gmail.com>
2021-04-11 22:03:43 +0600

    remove emoji character from source command

--- src/commands/misc/source.ts
+++ src/commands/misc/source.ts
@@ -39,7 +39,7 @@ export const command: Command = new Command(
             em.setColor(client.config.colors.normal)
                 .addField(
                     '\u200B',
-                    ` 📁 __[/src/commands/${cmd.category}/${cmd.name}.ts](${client.config.repoURL}/blob/main/src/commands/${cmd.category}/${cmd.name}.ts)__`
+                    `:file_folder:  __[/src/commands/${cmd.category}/${cmd.name}.ts](${client.config.repoURL}/blob/main/src/commands/${cmd.category}/${cmd.name}.ts)__`
                 )
                 .setDescription('`'.repeat(3) + 'ts\n' + file + '`'.repeat(3));
 
