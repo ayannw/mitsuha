9c556036819994521613c6d8519e86ab512a1f84
ayannw <ayanispro420@gmail.com>
2021-04-12 07:26:00 +0600

    v2.1.2-dev.1

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export const colors = {
     normal: '#3B88C3',
     warn: '#F69222',
     error: '#FF004A',
-    success: '#03FF00',
+    success: '#00FF7B',
 };
 export const repoURL = 'https://github.com/ayannw/mitsuha-snapshot';
