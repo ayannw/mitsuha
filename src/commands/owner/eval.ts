d74f7ec64722bf76d7277b9c8560dfd302012f7b
ayannw <ayanispro420@gmail.com>
2021-04-12 11:43:14 +0600

    update eval commnd

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -10,12 +10,17 @@ export const command: Command = new Command(
         aliases: ['ev'],
         category: 'owner',
         ownerOnly: true,
+        usage: 'eval [code]',
     },
     (client: MitsuhaClient, message: Message) => {
         const code = message.content.substr(message.content.indexOf(' ') + 1);
         const sw = new Stopwatch();
         const em: MessageEmbed = new MessageEmbed();
         let output: any;
+
+        if (code.trim() == message.content) {
+            return message.channel.send('I need some code to evaluate ...');
+        }
 
         sw.start();
         try {
@@ -51,7 +56,7 @@ export const command: Command = new Command(
                     '`'.repeat(3)
             )
             .setFooter(`Done in ${time}`, client.user.displayAvatarURL());
-		
+
         return message.channel.send(em);
     }
 );
