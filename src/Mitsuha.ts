26e5a7f804ff913e6cae9db392015764881cbfe6
ayannw <ayanispro420@gmail.com>
2021-04-19 17:52:15 +0600

    serve all commands and statistics to localhost

--- src/Mitsuha.ts
+++ src/Mitsuha.ts
@@ -1,4 +1,4 @@
-import * as server from './server';
+//import * as server from './server';
 import * as logger from '#lib/logger';
 import { __MitsuhaClient__, MitsuhaClient } from '#lib/MitsuhaClient';
 import { execCommand } from '#lib/utils/commandRunner';
@@ -6,6 +6,8 @@ import { Client } from 'discord.js';
 import { config } from 'dotenv';
 import { readFile } from 'fs';
 import { Stopwatch } from '@sapphire/stopwatch';
+import cors from 'cors';
+import express from 'express';
 
 config();
 
@@ -13,6 +15,31 @@ const _client: Client = new Client();
 const client: MitsuhaClient = __MitsuhaClient__(_client);
 const token = process.env.DISCORD_TOKEN;
 const sw = new Stopwatch();
+const app = express();
+
+app.use(cors());
+
+app.get('/api/commands', async (req, res) => {
+    const cmds = await client.commands;
+    const _list: any[] = [];
+
+    cmds.forEach((cmd) => {
+        _list.push({
+            name: cmd.name,
+            help: cmd.help,
+        });
+    });
+
+    const list = _list;
+
+    return res.json(list);
+});
+
+app.get('/api/stats', async (req, res) => {
+    const _stats = await client.stats();
+
+    return res.json(_stats);
+});
 
 const start = () => {
     console.clear();
@@ -23,7 +50,6 @@ const start = () => {
     });
 
     setTimeout((): void => {
-        server.start(Math.floor(Math.random() * 9999) + 1000);
         sw.start();
         try {
             client.login(token);
@@ -38,6 +64,14 @@ client.once('ready', () => {
     logger.success('logged in as ' + client.user.tag);
     const t = sw.stop().toString();
     logger.info('took ' + t + ' to login');
+
+    app.listen(process.env.API_PORT, () => {
+        logger.success(
+            'server started: http://0.0.0.0:' +
+                String(process.env.API_PORT) +
+                '/'
+        );
+    });
 });
 
 client.on('message', async (message) => {
